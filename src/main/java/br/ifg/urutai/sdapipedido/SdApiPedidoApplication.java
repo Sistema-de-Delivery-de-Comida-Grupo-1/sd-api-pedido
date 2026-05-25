622f1704c40d3098f111ecbfced7d92d89601fe3
josevictoraraujorojas <kaneki1212112121@gmail.com>
2026-05-25 09:27:32 -0300

    init

---
+++ src/main/java/br/ifg/urutai/sdapipedido/SdApiPedidoApplication.java
@@ -0,0 +1,13 @@
+package br.ifg.urutai.sdapipedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdApiPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdApiPedidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ifg/urutai/sdapipedido/SdApiPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifg.urutai.sdapipedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdApiPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
